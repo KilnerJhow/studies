8067a7e2272c21201d3c784c2c1f71a4b27c95d4
Jhow <jkss@cin.ufpe.br>
2018-05-08 10:43:06 -0300

    Modificação do background da questão 1 e alteração no modo como é calculo o algoritmo de jacobi na questão 2

--- Infra_software/Lista_threads/questao_1/q1mod.c
+++ Infra_software/Lista_threads/questao_1/q1mod.c
@@ -28,13 +28,22 @@
 #define N_THREADS 3
 #define TABLE_TAM 25
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
+
+#define ANSI_COLOR_RED     "\x1b[41m"
+#define ANSI_COLOR_GREEN   "\x1b[42m"
+#define ANSI_COLOR_YELLOW  "\x1b[43m"
+#define ANSI_COLOR_BLUE    "\x1b[44m"
+#define ANSI_COLOR_MAGENTA "\x1b[45m"
+#define ANSI_COLOR_CYAN    "\x1b[46m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define ANSI_COLOR_RED_1     "\x1b[31m"
+#define ANSI_COLOR_GREEN_1   "\x1b[32m"
+#define ANSI_COLOR_YELLOW_1 "\x1b[33m"
+#define ANSI_COLOR_BLUE_1    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA_1 "\x1b[35m"
+#define ANSI_COLOR_CYAN_1    "\x1b[36m"
+// #define ANSI_COLOR_RESET   "\x1b[0m"
 
 //Inicializamos um mutex para exclusão mútua de cada linha, já que vamos acessar um arquivo em comum
 static pthread_mutex_t line_mutex[MAX_LINES];
